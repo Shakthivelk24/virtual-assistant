d21fe14494b6c1bd5d61942ee8b725076b4d2e75
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-10 21:19:22 +0530

    updated the UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,19 +1,39 @@
-import React,{createContext} from 'react'
-
+import React, { createContext, useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 export const userDataContext = createContext();
 
-function userContext({children}) {
-    const serverUrl = "http://localhost:8000";
-    const value = {
-        serverUrl
-    };
+function UserContext({ children }) {
+  const serverUrl = "http://localhost:8000";
+  const [userData, setUserData] = React.useState(null);
+  const [frontendImage, setFrontendImage] = useState(null);
+  const [backendImage, setBackendImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/user/current`, {
+        withCredentials: true,
+      });
+      setUserData(result.data);
+      console.log("Current User Data:", result.data);
+    } catch (error) {
+      console.error("Error fetching current user data:", error);
+    }
+  };
+  useEffect(() => {
+    handleCurrentUser();
+  }, []);
+  const value = {
+    serverUrl,userData, setUserData,frontendImage,setFrontendImage,backendImage,setBackendImage,selectedImage,setSelectedImage
+  };
   return (
     <div>
-        <userDataContext.Provider value={value}>
+      <userDataContext.Provider value={value}>
         {children}
-        </userDataContext.Provider>
+      </userDataContext.Provider>
     </div>
-  )
+  );
 }
 
-export default userContext
+export default UserContext;
