452b966bf7149050a6c643b46164089e5c4cd7ec
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-10 21:17:13 +0530

    update App.mjx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
+import Customize from './pages/Customize.jsx'
+import Home from './pages/Home.jsx'
+import Customize2 from './pages/Customize2.jsx'
+import { userDataContext } from './context/UserContext.jsx'
+import { Navigate } from 'react-router-dom';
 
 function App() {
+  const {userData, setUserData} = useContext(userDataContext);
   return (
     <Routes>
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={(userData?.assistantImage && userData?.assistantName) ? <Home /> : <Navigate to={"/customize"} />} /> 
+      <Route path="/signin" element={!userData ? <SignIn /> : <Navigate to={"/"} />} />
+      <Route path="/signup" element={!userData ? <SignUp /> : <Navigate to={"/"} />} />
+      <Route path="/customize" element={userData ? <Customize /> : <Navigate to={"/signin"} />} />
+      <Route path="/customize2" element={userData ? <Customize2 /> : <Navigate to={"/signin"} />} />
     </Routes>
   )
 }
