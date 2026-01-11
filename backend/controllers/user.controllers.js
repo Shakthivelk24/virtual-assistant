b70e560ac6ee016ca7af5290bc31bced8752ec2b
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-11 21:11:49 +0530

    Updated the user.controllers.js for uploading the AssistantName and AssistantImage

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,14 +1,44 @@
-import User from '../models/user.models.js';
+import User from "../models/user.models.js";
+import uploadOnCloudinary from "../config/cloudinary.js";
 
 export const getCurrentUser = async (req, res) => {
-    try {
-        const userId = req.userId;
-        const user=await User.findById(userId).select('-password');
-        if(!user){
-            return res.status(404).json({message:"User not found"});
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({message:"Server error"});
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const updateAssistant = async (req, res) => {
+  try {
+    const { assistantName, imageUrl } = req.body;
+
+    let assistantImage = imageUrl;
+
+    if (req.file) {
+      const result = await uploadOnCloudinary(req.file.path);
+      assistantImage = result.secure_url;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.userId,
+      { assistantName, assistantImage },
+      { new: true }
+    ).select("-password");
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("UPDATE ERROR:", error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
