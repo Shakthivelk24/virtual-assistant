c8c5ea087a863f432072344d6b77121e5b0e1a90
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-12 22:53:43 +0530

    Designed the askToAssistant to the router

--- backend/routers/user.routes.js
+++ backend/routers/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user.controllers.js";
+import { getCurrentUser ,updateAssistant,askToAssistant} from "../controllers/user.controllers.js";
 import authMiddleware from "../middlewares/isAuth.js";
+import upload from "../middlewares/multer.js"
 
 const userRoutes = express.Router();
 
 userRoutes.get("/current",authMiddleware, getCurrentUser);
-
+userRoutes.put("/update",authMiddleware,upload.single('assistantImage'), updateAssistant);
+userRoutes.post("/ask",authMiddleware, askToAssistant);
 
 export default userRoutes;
