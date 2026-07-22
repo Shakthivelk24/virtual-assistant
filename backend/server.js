bf5afa00fb43b8cd2eafb4c29328a192929240ab
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-22 20:25:29 +0530

    updated the server.js with metrics for Prometheus

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import authRoutes from "./routers/auth.routes.js";
 import userRoutes from "./routers/user.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import register from "./metrics/metrics.js";
+import httpMetrics from "./metrics/httpMetrics.js";
 
 
 dotenv.config();
@@ -16,16 +18,42 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cookieParser()); // Middleware to parse cookies
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL, // Allow requests from the frontend URL
   credentials: true,
-}))
+}));
 
-const port = process.env.PORT || 5000;
+// ============================================================
+// Prometheus HTTP Metrics
+// ============================================================
+app.use(httpMetrics);
+
+const PORT = process.env.PORT || 5000;
+
+// ============================================================
+// Prometheus Metrics
+// ============================================================
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
 
 app.use("/api/auth", authRoutes); // Authentication routes
 app.use("/api/user", userRoutes); // User-related routes
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port http://localhost:${port}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server");
+    console.error(error);
+  }
+};
+
+startServer();
