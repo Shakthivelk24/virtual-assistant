ce740e924cfc6d0fe1f4c173916fe7bb0611e80c
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-04 21:15:55 +0530

    feat(tests): add Vitest configuration for testing and coverage reporting

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { coverageConfigDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   test: {
+    globals: true,
+    environment: 'jsdom',
+
+    setupFiles: './src/test/setup.js',
+
+    coverage: {
+      provider: 'v8',
+      reporter: [
+        'text',
+        'html',
+        'lcov'
+      ],
+
+      reportsDirectory: './coverage',
+
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'src/main.jsx'
+      ]
+    }
+  }
 })
