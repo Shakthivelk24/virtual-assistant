df9a23216174205343605d4120203d2c4f02662f
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-08-06 21:50:12 +0530

    refactor: simplify server setup by removing unused middleware and routes

--- server/server.js
+++ server/server.js
@@ -1,57 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import authRoutes from "./routers/auth.routes.js";
-import userRoutes from "./routers/user.routes.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import register from "./metrics/metrics.js";
-import httpMetrics from "./metrics/httpMetrics.js";
-
+import app from "./app.js";
 
 dotenv.config();
 
-connectDB();
-
-const app = express();
-
-app.use(express.json()); // Middleware to parse JSON request bodies
-app.use(cookieParser()); // Middleware to parse cookies
-app.use(cors({
-  origin: process.env.FRONTEND_URL, // Allow requests from the frontend URL
-  credentials: true,
-}));
-
-// ============================================================
-// Prometheus HTTP Metrics
-// ============================================================
-app.use(httpMetrics);
-
 const PORT = process.env.PORT || 5000;
-
-// ============================================================
-// Prometheus Metrics
-// ============================================================
-
-app.get("/metrics", async (req, res) => {
-  res.set("Content-Type", register.contentType);
-  res.end(await register.metrics());
-});
-
-// ============================================================
-// Health Check
-// ============================================================
-app.get("/api/health", (req, res) => {
-  res.status(200).json({
-    status: "UP",
-    service: "backend",
-  });
-});
-
-
-app.use("/api/auth", authRoutes); // Authentication routes
-app.use("/api/user", userRoutes); // User-related routes
-
 
 const startServer = async () => {
   try {
